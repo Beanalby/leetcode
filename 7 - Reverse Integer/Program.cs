fb7e845bd049a2f9da6b9e9aaeaec5091e63d926
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-06-20 00:54:22 -0400

    7 Done, surprisingly straightforward

---
+++ 7 - Reverse Integer/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _7___Reverse_Integer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s = new Solution();
+        int[] problems = new int[] {1, 0, -1, 10, 01, 101000, 123, -123, 120, Int32.MaxValue, Int32.MinValue};
+        foreach(int problem in problems) {
+            Console.WriteLine($"{problem} -> {s.Reverse(problem)}");
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Solution {
+    public int Reverse(int x) {
+        bool negate = false;
+        if(x < 0) {
+            x = -x;
+            negate = true;
+        }
+
+        String s = x.ToString();
+        char[] array = s.ToCharArray();
+        Array.Reverse(array);
+        s = new string(array);
+
+        System.Int32 result;
+        if(!System.Int32.TryParse(s, out result)) {
+            result = 0;
+        }
+        if(negate) {
+            result = -result;
+        }
+        return result;
+    }
+}
