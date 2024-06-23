4f80d84f907a0a7c8a0f2ab025e075d652734b13
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-06-22 23:07:56 -0400

    '9 Palindrome Number' done, easy problem is easy

---
+++ 9 - Palindrome Number/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program {
+    static void Main(string[] args) {
+        int[] problems = new int[] { 121, -121, 10};
+        Solution s = new Solution();
+        foreach(var problem in problems) {
+            Console.WriteLine($"{problem} -> {s.IsPalindrome(problem)}");
+        }
+    }
+}
+
+public class Solution {
+    public bool checkPalindrome(string s, int leftPos, int rightPos) {
+        while(true) {
+            if(leftPos == -1 && rightPos == s.Length) {
+                return true;
+            }
+            if(leftPos <=-1 || rightPos >= s.Length) {
+                return false;
+            }
+            if(s[leftPos] != s[rightPos]) {
+                return false;
+            }
+            leftPos--;
+            rightPos++;
+        }
+    }
+    public bool IsPalindrome(int x) {
+        if(x<0) {
+            return false;
+        }
+        var xStr = x.ToString();
+        if(xStr.Length%2 == 0) {
+            return checkPalindrome(xStr, (xStr.Length/2)-1, xStr.Length/2);
+        } else {
+            return checkPalindrome(xStr, (xStr.Length/2)-1, (xStr.Length/2)+1);
+        }
+    }
+}
