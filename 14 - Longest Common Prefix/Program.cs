7ddd04b3e3321535216e87a54cc08bd9fd952f5e
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-07-12 19:34:48 -0400

    Did 14 Longest Common Prefix, pretty easy

---
+++ 14 - Longest Common Prefix/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _14___Longest_Common_Prefix;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution s = new();
+        string[][] problems = [
+            ["","","",""],
+            ["foo"],
+            ["ab","a"],
+            ["flower","flow","flight"],
+            ["abc","def","ghij"],
+            ];
+        foreach(var problem in problems) {
+            Console.WriteLine($"[{String.Join(",",problem)}] -> {s.LongestCommonPrefix(problem)}");
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        // short-circuit a single string
+        if(strs.Length == 1) {
+            return strs[0];
+        }
+        int minLength = strs.Aggregate(int.MaxValue, (i,s) => Math.Min(i,s.Length));
+        if(minLength==0) {
+            return "";
+        }
+        int i = 0;
+        string s = strs[0];
+        bool sawInvalid = false;
+        for(i=0;i<minLength && !sawInvalid;i++) {
+            for(int strNum=1;strNum<strs.Length;strNum++) {
+                if(strs[strNum][i] != s[i]) {
+                    sawInvalid = true;
+                    i--;
+                    break;
+                }
+            }
+        }
+        return s[..i];
+    }
+}
