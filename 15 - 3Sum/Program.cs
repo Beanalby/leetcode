43a189eee5a077d4ef47336d288d84df390a81c9
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-07-13 13:57:04 -0400

    Rejiggered 15's algorithm, removed custom binSearch and instead moves two fenceposts, with a couple early abort checks.  Much better!

--- 15 - 3Sum/Program.cs
+++ 15 - 3Sum/Program.cs
@@ -36,59 +36,36 @@ class Program
         }
         Console.WriteLine("Hello, World!");
     }
-    static void binTest() {
-        Solution s = new();
-        int[] nums = [-1, 0, 1, 2, -1, -4];
-        Array.Sort(nums);
-        for(int i=-5;i<4;i++) {
-            Console.WriteLine($"{i} -> {s.binSearch(nums, 3, i)}");
-        }
-    }
 }
 
 public class Solution {
-    // Returns the index of the target element, -1 if not found
-    public int binSearch(int[] nums, int startPos, int target) {
-        int lower = startPos, upper = nums.Length-1, current;
-        while(true) {
-            current = (lower + upper) / 2;
-            if(nums[current] == target) {
-                return current;
-            }
-            if(nums[current] < target) {
-                // because of floor on integer division,
-                // let lower to increase even if it's current,
-                // as long as it's not upper
-                if(lower == upper) {
-                    return -1;
-                }
-                // accomplishing the setup for the above test,
-                // let lower go to 1 more than current if it's
-                // current at the moment.
-                lower = (lower == current) ? current+1 : current;
-            } else {
-                if(upper == current) {
-                    return -1;
-                }
-                upper = current;
-            }
-        }
-    }
 
     public IList<IList<int>> ThreeSum(int[] nums) {
         HashSet<(int,int,int)> result = new();
         Array.Sort(nums);
-        for(int i=0;i<nums.Length-2; i++) {
-            for(int j=i+1; j<nums.Length-1; j++) {
-                var target = -(nums[i] + nums[j]);
-                // don't bother looking if the target
-                // is lower than either number - we would've already
-                // found this triplet when "target" was num1 or num2.
-                if(target < nums[i] || target < nums[j]) {
-                    continue;
-                }
-                if(-1 != binSearch(nums, j+1, target)) {
-                    result.Add( (nums[i], nums[j], target));
+        int j,k, sum, len=nums.Length;
+        
+        // stop early when nums[i] is positive - can't have
+        // a solution involving 3 positive numbers
+        for(int i=0;i<nums.Length-2 && nums[i] <= 0; i++) {
+            // if this is the same number as the previous one,
+            // we already found all the solutions where this is
+            // the first number
+            if(i>0 && nums[i]==nums[i-1]) {
+                continue;
+            }
+            j=i+1;
+            k=len-1;
+            while(j < k) {
+                sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0) {
+                    result.Add( (nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if(sum < 0) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
