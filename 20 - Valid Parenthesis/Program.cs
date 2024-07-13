152b77afcae07b4a484cf291ec625673b20e9d68
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-07-13 18:35:53 -0400

    Implemented 20 - Valid Parenthesis, e.g. 'Do you konw what a stack is'

---
+++ 20 - Valid Parenthesis/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _20___Valid_Parenthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution s = new();
+        string[] problems = [
+            "([{()[]}][])",
+            "()[]{}",
+            "(]",
+        ];
+        foreach(var p in problems) {
+            Console.WriteLine($"{p} -> {s.IsValid(p)}");
+        }
+    }
+}
+
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new();
+        foreach(char c in s) {
+            switch(c) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.Push(c);
+                    break;
+                case ')':
+                    if(stack.Pop()!='(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(stack.Pop()!='[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(stack.Pop()!='{') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.Count==0;
+    }
+}
