3ae99154f6a7e8a310da9e82cdd50ab04fabacb2
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-07-14 12:53:42 -0400

    Did 28 Find the Index of the First Occurrence in a String, quite possibly the easiest problem so far.  Feels more like a C question, and would still be easy

---
+++ 28 Find the Index of the First Occurrence in a String/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _28_Find_the_Index_of_the_First_Occurrence_in_a_String;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        (string haystack, string needle)[] problems = [
+            ("a", "b"),
+            ("sadbutsad", "sad"),
+            ("leetcode", "leeto"),
+            ("foobarbaz", "bar"),
+        ];
+        Solution s = new();
+        foreach(var (haystack, needle) in problems) {
+            Console.WriteLine($"{haystack}, {needle} -> {s.StrStr(haystack,needle)}");
+        }
+    }
+}
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        return haystack.IndexOf(needle);
+    }
+}
