8d0dacb3fc955bd53e769a1b592564a4821f4169
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-07-13 20:55:55 -0400

    Improved 23 by removing LINQ, went from 1015ms to 89ms 😲

--- 23 Merge k Sorted Lists/Program.cs
+++ 23 Merge k Sorted Lists/Program.cs
@@ -67,16 +67,43 @@ public class Solution {
 
         ListNode dummy = new ListNode(-1);
         ListNode? current = dummy;
+        int i;
+        bool sawNonNull;
+        int lowest, lowestCount;
         while(true) {
-            // check if we're done
-            if(!lists.Any((ListNode? ln) => ln!=null)) {
+            #region check if we're done
+            sawNonNull = false;
+            for(i=0;i<lists.Length; i++) {
+                if(lists[i]!=null) {
+                    sawNonNull = true;
+                    break;
+                }
+            }
+            if(!sawNonNull) {
                 return dummy.next;
             }
-            // find the lowest value among the list nodes
-            int lowest = lists.Aggregate(int.MaxValue, (int x, ListNode? ln) => ln==null ? x : ln.val < x ? ln.val : x);
-            // find ** list that has the lowest value, and advance it.
-            // We don't care which one it is.
-            for(int i=0;i<lists.Length;i++) {
+            #endregion
+
+            #region find the lowest value among the list nodes
+            lowest = int.MaxValue;
+            lowestCount=0;
+            for(i=0;i<lists.Length;i++) {
+                #pragma warning disable CS8602 // Dereference of a possibly null reference.
+                if(lists[i]!=null) {
+                    if(lists[i].val == lowest) {
+                        lowestCount++;
+                    }
+                    if(lists[i].val < lowest) {
+                        lowest = lists[i].val;
+                        lowestCount=1;
+                    }
+                }
+                #pragma warning restore CS8602 // Dereference of a possibly null reference.
+            }
+            #endregion 
+
+            #region find *SOME* list that has the lowest value, and advance it. We don't care which one it is.
+            for(i=0;i<lists.Length;i++) {
                 if(lists[i] == null) {
                     continue;
                 }
@@ -87,10 +114,13 @@ public class Solution {
                         current = current?.next;
                     }
                     lists[i] = lists[i]?.next;
-                    // found the list we're advanding, don't need to look at rest
-                    break;
+                    // see if we have any more lists to advance
+                    if(--lowestCount == 0) {
+                        break;
+                    }
                 }
             }
+            #endregion
         }
     }
 }
